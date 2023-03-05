0d2dd021caa2d2c624403074fe8a8846dbe1f84c
kaisehm <kais@hello-charly.com>
2023-03-05 16:16:02 +0100

    init spring-boot project

---
+++ jwtSecure/src/main/java/com/kais/jwtSecure/JwtSecureApplication.java
@@ -0,0 +1,13 @@
+package com.kais.jwtSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecureApplication.class, args);
+	}
+
+}
---
+++ jwtSecure/src/test/java/com/kais/jwtSecure/JwtSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kais.jwtSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
